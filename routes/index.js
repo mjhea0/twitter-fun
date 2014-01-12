4eaf767510e57e866e31b8e0dbb3a5e63bfd010e
Michael Herman <hermanmu@gmail.com>
2014-01-12 01:16:42 -0700

    added an iffe to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,50 +1,54 @@
-var path = require("path");
-var twitter = require('../config.js');
+var test = (function(){
 
-exports.index = function(req, res){
-  res.render('twit', { title: "Twitter Fun"});
-};
+  var path = require("path"),
+      twitter = require('../config.js');
 
-exports.ping = function(req, res){
-  res.send("pong!", 200);
-};
+  exports.index = function(req, res){
+    res.render('twit', { title: "Twitter Fun"});
+  };
 
-exports.search = function(req, res){
-  // search twitter api for friends
-  var username = req.query.search,
-      friends = [],
-      test = [];
-  
-  twitter.get('friends/list', { screen_name: username, count: 200 }, function(err, data){
-    if(err) {
-      res.send("<br>Error! Rate limit exceeded.");
-      console.log(err)
-      return;
-    };
+  exports.ping = function(req, res){
+    res.send("pong!", 200);
+  };
 
-    for (var i = 0; i < (data.users).length ; i++) {
-      // search twitter for info about the relationship between two users
-      twitter.get('friendships/show', {source_screen_name: username, target_screen_name: data.users[i].screen_name}, 
-        function(error, results){
-          if(err) {
-            res.send("<br>Error! Rate limit exceeded.");
-            console.log(err)
-            return;
-          };
-          // add friend to array
-          friends.push(results.relationship.target.screen_name)
-          // add whether that friend is following you to array
-          friends.push((results.relationship.target.following).toString())
-          test.push(results.relationship.target)
-          // continue to loop until all data is added to the array
-          if (friends.length === ((data.users).length + (data.users).length)) {
-            console.log(test)
-            res.send(test)
-          };
-        });
-    };
-  })
-};
+  exports.search = function(req, res){
+    // search twitter api for friends
+    var username = req.query.search,
+        friends = [],
+        test = [];
+    
+    twitter.get('friends/list', { screen_name: username, count: 200 }, function(err, data){
+      if(err) {
+        res.send("<br>Error! Rate limit exceeded.");
+        console.log(err)
+        return;
+      };
+
+      for (var i = 0; i < (data.users).length ; i++) {
+        // search twitter for info about the relationship between two users
+        twitter.get('friendships/show', {source_screen_name: username, target_screen_name: data.users[i].screen_name}, 
+          function(error, results){
+            if(err) {
+              res.send("<br>Error! Rate limit exceeded.");
+              console.log(err)
+              return;
+            };
+            // add friend to array
+            friends.push(results.relationship.target.screen_name)
+            // add whether that friend is following you to array
+            friends.push((results.relationship.target.following).toString())
+            test.push(results.relationship.target)
+            // continue to loop until all data is added to the array
+            if (friends.length === ((data.users).length + (data.users).length)) {
+              console.log(test)
+              res.send(test)
+            };
+          });
+      };
+    })
+  };
+
+}(test));
 
 // add promises, functions
 // $.when(promise)
